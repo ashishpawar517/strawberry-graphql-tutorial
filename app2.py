cf9cff41b321699f7b9942e278cae36ff3a4dfe4
Aashish <pawarashish564@gmail.com>
2021-03-14 14:41:45 +0530

    app2 commenting & code removal

--- app2.py
+++ app2.py
@@ -1,3 +1,5 @@
+# Tutorial 2
+
 # creating a schema
 
 # graphql schema is used to define the structure of the data that client
@@ -6,19 +8,15 @@ import typing
 import strawberry 
 import dataclasses
 
+
+# BOOK API
 @strawberry.type
 class Book:
     title:str
     author:str
 
-# creating a query books that will return list of zero or more
-# books 
-
-
 # strawberry can work with any times of data rest,db
 # here we are creating a hardcoded data
-
-
 db = {
     "1":Book(title = 'JAVA PRO 8',author = "JVM Gosling"),
     "2":Book(title = 'PYTHON magic ',author = 'anaconda'),
@@ -29,24 +27,21 @@ db = {
 def get_books():
     return list(db.values())
 
-def get_book_by_id(root,n:strawberry.ID) -> typing.Optional[Book]:
+def get_book_by_id(n:strawberry.ID) -> typing.Optional[Book]:
     return db.get(n)
 
+# query apis - books for all , get_book_by_id for getting specific book
 @strawberry.type
 class Query:
     # books:typing.List(Book)
     books: typing.List[Book] = strawberry.field(resolver=get_books)
     get_books_by_id : typing.Optional[Book] = strawberry.field(resolver = get_book_by_id)
 
-@strawberry.input
-class BookInput:
-    title:str
-    author:str
-
+# mutation for adding new data
 @strawberry.type
 class Mutation:
     @strawberry.mutation
-    def create_book(self,info,title:str,author:str) -> Book:
+    def create_book(self,title:str,author:str) -> Book:
         last = int(list(db.keys())[-1])
         last += 1
         db[str(last)] = Book(title,author)
