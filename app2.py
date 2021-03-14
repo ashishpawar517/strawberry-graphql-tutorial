d5e64da20570f20c5003dc2cbc35898b7151d2b5
Aashish <pawarashish564@gmail.com>
2021-03-14 11:30:06 +0530

    mutations done

--- app2.py
+++ app2.py
@@ -4,6 +4,7 @@
 # can query
 import typing
 import strawberry 
+import dataclasses
 
 @strawberry.type
 class Book:
@@ -17,18 +18,38 @@ class Book:
 # strawberry can work with any times of data rest,db
 # here we are creating a hardcoded data
 
+
+db = {
+    "1":Book(title = 'JAVA PRO 8',author = "JVM Gosling"),
+    "2":Book(title = 'PYTHON magic ',author = 'anaconda'),
+    "3":Book(title = 'colorful java', author = 'gosling'),
+}
+
+
 def get_books():
-    return [
-        Book(
-            title = "The Great SQL - GraphQL",
-            author = "F. Agr author ",
-        )
-    ]
+    return list(db.values())
+
+def get_book_by_id(root,n:strawberry.ID) -> typing.Optional[Book]:
+    return db.get(n)
 
 @strawberry.type
 class Query:
     # books:typing.List(Book)
     books: typing.List[Book] = strawberry.field(resolver=get_books)
+    get_books_by_id : typing.Optional[Book] = strawberry.field(resolver = get_book_by_id)
 
+@strawberry.input
+class BookInput:
+    title:str
+    author:str
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def create_book(self,info,title:str,author:str) -> Book:
+        last = int(list(db.keys())[-1])
+        last += 1
+        db[str(last)] = Book(title,author)
+        return Book(title,author)
+
+schema = strawberry.Schema(query=Query,mutation=Mutation)
